1c5e5eeb99d147662870ad9cb7319bd21a841333
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-03-11 20:26:50 -0400

    - Added closing checks, - polished add logic, - added a way to keep track of if data has been edited or not - Made it so that IDs can not be edited from the data grid to maintain PK integrity !!

--- P01-ConfigTool/P01-ConfigTool/ConfigurationItem.cs
+++ P01-ConfigTool/P01-ConfigTool/ConfigurationItem.cs
@@ -2,10 +2,72 @@
 {
     public class ConfigurationItem
     {
+        private string _configName = string.Empty;
+        private string _configValue = string.Empty;
+        private string _dataType = "string";
+        private string _description = string.Empty;
+        private bool _isLoaded = false; // track loading state
+
         public int ConfigID { get; set; }
-        public string ConfigName { get; set; } = string.Empty;
-        public string ConfigValue { get; set; } = string.Empty;
-        public string DataType { get; set; } = "string";
-        public string? Description { get; set; }
+
+        public string ConfigName
+        {
+            get => _configName;
+            set
+            {
+                _configName = value ?? string.Empty; // set default if null
+                if (_isLoaded)
+                {
+                    NeedsSaving = true; // update needs saving flag if edited
+                }
+            }
+        }
+
+        public string ConfigValue
+        {
+            get => _configValue;
+            set
+            {
+                _configValue = value ?? string.Empty; // set default if null
+                if (_isLoaded)
+                {
+                    NeedsSaving = true; // update needs saving flag if edited
+                }
+            }
+        }
+
+        public string DataType
+        {
+            get => _dataType;
+            set
+            {
+                _dataType = value ?? "string"; // set default if null
+                if (_isLoaded)
+                {
+                    NeedsSaving = true; // update needs saving flag if edited
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                _description = value ?? string.Empty; // set default if null
+                if (_isLoaded)
+                {
+                    NeedsSaving = true; // update needs saving flag if edited
+                }
+            }
+        }
+
+        public bool NeedsSaving { get; set; } = false; // set false to start
+
+        // call after populating data grid so that it knows not to set needs saving flags for loads
+        public void MarkLoaded()
+        {
+            _isLoaded = true;
+        }
     }
 }
--- P01-ConfigTool/P01-ConfigTool/MainWindow.xaml.cs
+++ P01-ConfigTool/P01-ConfigTool/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿//------------------------------------------------------------------------------
 // FILE          : MainWindow.xaml.cs
 // PROJECT       : Project ManufacturingP01 - ConfigTool (Milestone 1)
-// PROGRAMMER    : Bibi Murwared, Julia Jakob
+// PROGRAMMERS    : Bibi Murwared, Julia Jakob
 // FIRST VERSION : 2026-03-11
 // DESCRIPTION   : Code-behind for the configuration tool. Uses ADO.NET
 //                 to load, add, edit, delete and reset configuration values
@@ -9,6 +9,8 @@
 //------------------------------------------------------------------------------
 //https://learn.microsoft.com/en-us/dotnet/desktop/wpf/data/how-to-create-and-bind-to-an-observablecollection
 //https://learn.microsoft.com/en-us/dotnet/api/system.data.sqlclient.sqlcommand.executescalar?view=netframework-4.8.1
+//https://learn.microsoft.com/en-us/dotnet/api/system.linq.enumerable.any?view=net-10.0
+
 using Microsoft.Data.SqlClient;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
@@ -33,6 +35,7 @@ namespace P01_ConfigTool
     public partial class MainWindow : Window
     {
         private string connectionString;
+        private bool userConfirmedClose = false;
 
         //in memory collection bound to the datagrid
         private ObservableCollection<ConfigurationItem> configurations;
@@ -43,6 +46,7 @@ namespace P01_ConfigTool
             // get connection string from config file
             connectionString = ConfigurationManager.ConnectionStrings["JubiConnection"].ConnectionString;
             LoadConfigurations(); // load data from database into the collection and grid
+            Closing += MainWindow_Closing; // hook up the x button so it checks for the saves too
 
         }
 
@@ -59,7 +63,7 @@ namespace P01_ConfigTool
             using (SqlConnection connection = new SqlConnection(connectionString))
 
             //read all configuration rows
-            using (SqlCommand command = new SqlCommand("SELECT ConfigID, ConfigName, ConfigValue, DataType, Description FROM Configuration ORDER BY ConfigName",connection))
+            using (SqlCommand command = new SqlCommand("SELECT ConfigID, ConfigName, ConfigValue, DataType, Description FROM Configuration ORDER BY ConfigName", connection))
             {
                 connection.Open();
 
@@ -86,6 +90,12 @@ namespace P01_ConfigTool
             // bind the collection to the datagrid
             ConfigurationsDisplay.ItemsSource = configurations;
 
+            // indicate that info has been loaded so that it knows the data is loaded and not changed data
+            foreach (ConfigurationItem item in configurations)
+            {
+                item.MarkLoaded();
+            }
+
             // make it so that configs are ordered by ID
             var view = (ListCollectionView)CollectionViewSource.GetDefaultView(configurations);
             view.SortDescriptions.Clear();
@@ -93,7 +103,7 @@ namespace P01_ConfigTool
 
             DeleteButton.IsEnabled = false;  // disable delete button until a row is selected
         }
-      
+
         /// <summary>
         /// handles selection changes in the datagrid and enables or disables the delete button
         /// </summary>
@@ -114,7 +124,7 @@ namespace P01_ConfigTool
         private void ResetToDefaults_Click(object sender, RoutedEventArgs e)
         {
             // ask the user to confirm the reset
-            MessageBoxResult result = MessageBox.Show("Reset all configuration values to defaults?\n (WARNING: This will DELETE any non-default created rows!!)","Confirm reset", MessageBoxButton.YesNo,MessageBoxImage.Warning);
+            MessageBoxResult result = MessageBox.Show("Reset all configuration values to defaults?\n (WARNING: This will DELETE any non-default created rows!!)", "Confirm reset", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
             if (result != MessageBoxResult.Yes)
             {
@@ -166,9 +176,8 @@ namespace P01_ConfigTool
         /// <param name="sender">the close button</param>
         /// <param name="e">event data for the click</param>
         private void Close_Click(object sender, RoutedEventArgs e)
-        {  
-            Close();
-
+        {
+            CheckUnsavedChangesAndClose();
         }
 
         /// <summary>
@@ -199,7 +208,7 @@ namespace P01_ConfigTool
                             insertCommand.Parameters.AddWithValue("@name", item.ConfigName);
                             insertCommand.Parameters.AddWithValue("@value", item.ConfigValue);
                             insertCommand.Parameters.AddWithValue("@type", item.DataType);
-                            insertCommand.Parameters.AddWithValue("@description",(object?)item.Description ?? DBNull.Value);
+                            insertCommand.Parameters.AddWithValue("@description", (object?)item.Description ?? DBNull.Value);
 
                             //ExecuteScalar returns the new identity value
                             object newId = insertCommand.ExecuteScalar();
@@ -213,19 +222,21 @@ namespace P01_ConfigTool
                             updateCommand.Parameters.AddWithValue("@id", item.ConfigID);
                             updateCommand.Parameters.AddWithValue("@value", item.ConfigValue);
                             updateCommand.Parameters.AddWithValue("@type", item.DataType);
-                            updateCommand.Parameters.AddWithValue("@description",(object?)item.Description ?? DBNull.Value);
+                            updateCommand.Parameters.AddWithValue("@description", (object?)item.Description ?? DBNull.Value);
 
                             updateCommand.ExecuteNonQuery();
                         }
+                        item.NeedsSaving = false; // item has been saved so flag is reset to false
                     }
                 }
                 // show the confirmation
-                MessageBox.Show("Configurations saved.","Success", MessageBoxButton.OK, MessageBoxImage.Information);
+                MessageBox.Show("Configurations saved.", "Success", MessageBoxButton.OK, MessageBoxImage.Information);
+
             }
             catch (Exception error)
             {
                 //show error if something goeas wrong
-                MessageBox.Show("Error saving configurations:\n" + error.Message,"Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                MessageBox.Show("Error saving configurations:\n" + error.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
 
@@ -245,7 +256,7 @@ namespace P01_ConfigTool
             }
 
             //confirm delete with the user
-            MessageBoxResult result = MessageBox.Show($"Delete configuration '{selectedItem.ConfigName}'?","Confirm delete",MessageBoxButton.YesNo,MessageBoxImage.Warning);
+            MessageBoxResult result = MessageBox.Show($"Delete configuration '{selectedItem.ConfigName}'?", "Confirm delete", MessageBoxButton.YesNo, MessageBoxImage.Warning);
 
             if (result != MessageBoxResult.Yes)
             {
@@ -256,7 +267,7 @@ namespace P01_ConfigTool
             if (selectedItem.ConfigID != 0)
             {
                 using (SqlConnection connection = new SqlConnection(connectionString))
-                using (SqlCommand command = new SqlCommand("DELETE from Configuration WHERE ConfigID = @id",connection))
+                using (SqlCommand command = new SqlCommand("DELETE from Configuration WHERE ConfigID = @id", connection))
                 {
                     command.Parameters.AddWithValue("@id", selectedItem.ConfigID);
                     connection.Open();
@@ -267,5 +278,69 @@ namespace P01_ConfigTool
             //remove the item from the observable collection
             configurations.Remove(selectedItem);
         }
+
+        /// <summary>
+        /// Check for saving if the main window's X is clicked too
+        /// </summary>
+        /// <param name="sender">object that raised the event.</param>
+        /// <param name="e">Event data for the Loaded event.</param>
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (userConfirmedClose)
+            {
+                return; // Allow the close to proceed
+            }
+
+            CheckUnsavedChangesAndClose();
+            e.Cancel = true; // Prevents close until user confirms
+        }
+
+        /// <summary>
+        /// Check to see if anything needs saving before closing!
+        /// </summary>
+        private void CheckUnsavedChangesAndClose()
+        {
+            bool hasUnsavedChanges = configurations.Any(item => item.NeedsSaving || item.ConfigID == 0); // check to see if any items in the data set have the NeedsSaving flag set to true or are new
+
+            // if nothing needs saving > close window
+            if (!hasUnsavedChanges)
+            {
+                userConfirmedClose = true;
+                Close();
+                return;
+            }
+
+            // show message box before closing to confirm close with user first
+            MessageBoxResult result = MessageBox.Show("You have unsaved changes.\nSave before closing?",
+                "ConfigTool - Unsaved Changes", MessageBoxButton.YesNoCancel,
+                MessageBoxImage.Warning, MessageBoxResult.No);
+
+            // switch the result to determine what action should be taken
+            switch (result)
+            {
+                // if they want to save
+                case MessageBoxResult.Yes:
+                    Save_Click(null, null); // call Save Click with null values for object and sender
+                    // double check to make sure it saved and close
+                    if (!configurations.Any(item => item.NeedsSaving || item.ConfigID == 0))
+                    {
+                        userConfirmedClose = true;
+                        Close();
+                    }
+                    break;
+
+                // if they don't want to save
+                case MessageBoxResult.No:
+                    userConfirmedClose = true;
+                    Close();
+                    break;
+
+                case MessageBoxResult.Cancel:
+                    break; // break without closing
+
+            }
+        }
+
+
     }
 }
