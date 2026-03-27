66683df7f7be823dffe20d7d2814a0a5fb23f43f
juliajakob370 <jjakob3896@conestogac.on.ca>
2026-03-27 01:46:48 -0400

    finished basic UI for workstation simulation - ready to work on tomorro

---
+++ P01-WorkstationSimulation/P01-WorkstationSimulation/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace P01_WorkstationSimulation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StopButton.IsEnabled = true;
+            StartButton.IsEnabled = false;
+
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+        }
+    }
+}
